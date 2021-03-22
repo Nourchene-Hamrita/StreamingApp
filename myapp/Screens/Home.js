224357ce582cf888f8940af5372788ffaf107ae7
Nourchene Hamrita <hamritanourchene@gmail.com>
2021-03-22 16:11:33 +0100

    commit

--- myapp/Screens/Home.js
+++ myapp/Screens/Home.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { createStackNavigator} from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import Header from '../Screens/Header';
 const HomeStack = createStackNavigator();
 const HomeStackScreen = () => {
-    return (
-        <HomeStack.Navigator>
-            <HomeStack.Screen name="Home" component={Home} options={({ navigation }) => ({
-                headerTitle: () => (<Header navigation={navigation} title='Home' />)
-            })} />
-        </HomeStack.Navigator>)
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={Home} options={({ navigation }) => ({
+        headerTitle: () => (<Header navigation={navigation} title='Home' />)
+      })} />
+    </HomeStack.Navigator>)
 }
 
- class Home extends Component {
+class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -23,12 +23,12 @@ const HomeStackScreen = () => {
   componentDidMount() {
   }
 
-   render() {
-     return (
-       <View style={{ flex: 1 }}>
-         <Text>MyScreen</Text>
-       </View>
-     );
-   }
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Text>MyScreen</Text>
+      </View>
+    );
+  }
 }
 export default HomeStackScreen;
